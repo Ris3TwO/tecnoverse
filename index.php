55f340709706c7775ecb68e71d7bb4cd75ca629e
Ris3TwO <ris3two@gmail.com>
2019-08-31 21:30:54 -0300

    Added

--- index.php
+++ index.php
@@ -518,7 +518,7 @@
                                 <p>
                                     <small>Jorgelis Chávez</small>
                                 </p>
-                                <p>
+                                <p style="text-align:justify;">
                                     La NASA desarrolló una nueva tecnología para diagnosticar enfermedades, como el cáncer, antes de que las persona la padezcan. Esta nueva modalidad es diseñada empleando, robots miniatura (nanotecnología).
                                 </p>
                                 <a href="./articles/tecnologia-curativa-microbots-medicos" class="btn btn-sm btn-default"><span class="glyphicon glyphicon-new-window"></span> Leer más</a>
